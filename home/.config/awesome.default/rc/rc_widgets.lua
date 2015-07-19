127aa989642e552915413dc2faf4ea47f6ce351c
vonpupp <vonpupp@gmail.com>
2015-07-19 20:58:43 -0300

    awesome: added volume text indicator

--- home/.config/awesome.default/rc/rc_widgets.lua
+++ home/.config/awesome.default/rc/rc_widgets.lua
@@ -82,6 +82,7 @@ for s = 1, screen.count() do
     local right_layout = wibox.layout.fixed.horizontal()
     if s == 1 then right_layout:add(wibox.widget.systray()) end
     right_layout:add(xkbmap.widget)
+    right_layout:add(volume_widget)
     right_layout:add(mytextclock)
     right_layout:add(mylayoutbox[s])
 
